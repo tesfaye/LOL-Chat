bb4c36ec1a229eae75baa8c9d18bc8be5d07afba
Abel <bariebe@disney.com>
2014-08-25 09:41:20 -0400

    removed more crap

--- app/src/main/java/com/tesfaye/lolchat/ChatService.java
+++ app/src/main/java/com/tesfaye/lolchat/ChatService.java
@@ -1,19 +1,14 @@
 package com.tesfaye.lolchat;
 
 import android.app.Notification;
-import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
-import android.os.Bundle;
 import android.os.IBinder;
-import android.util.Log;
 
 import com.github.theholywaffle.lolchatapi.ChatServer;
 import com.github.theholywaffle.lolchatapi.FriendRequestPolicy;
 import com.github.theholywaffle.lolchatapi.LolChat;
-
-import java.util.logging.Logger;
 
 /**
  * Created by Abel Tesfaye on 8/23/2014.
@@ -54,15 +49,15 @@ public class ChatService extends Service{
     }
     @Override
     public void onDestroy() {
-       super.onDestroy();
-       if(lolChat != null) {
-           new Thread(new Runnable() {
-               @Override
-               public void run() {
-                   lolChat.disconnect();
-               }
-           }).start();
-       }
+        super.onDestroy();
+        if(lolChat != null) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    lolChat.disconnect();
+                }
+            }).start();
+        }
     }
     public class LocalBinder extends Binder {
         ChatService getService() {
