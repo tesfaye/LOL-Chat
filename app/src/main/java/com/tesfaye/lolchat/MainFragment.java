ae968db1ed11f8577deecdbd1887a88571c53ddb
Abel <abeln8r@cox.net>
2014-08-26 11:21:24 -0400

    listview test

--- app/src/main/java/com/tesfaye/lolchat/MainFragment.java
+++ app/src/main/java/com/tesfaye/lolchat/MainFragment.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainFragment extends Fragment
 {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_lolchat_main, container, false);
+        ListView listView = (ListView) view.findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter(getActivity(), R.layout.friend_row, new ArrayList()));
         return view;
     }
 }
