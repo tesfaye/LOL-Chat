9de1b5afedd8c109d504b5e34adae7a117fa51e4
Abel <abeln8r@cox.net>
2014-08-29 11:45:41 -0400

    cleanup

--- app/src/main/java/com/tesfaye/lolchat/MainFragment.java
+++ app/src/main/java/com/tesfaye/lolchat/MainFragment.java
@@ -3,7 +3,6 @@ package com.tesfaye.lolchat;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
