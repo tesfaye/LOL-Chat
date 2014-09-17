80aa07bb50a27adb9d43bfebb69d399d0d278871
Abel Tesfaye <abeln8r@cox.net>
2014-09-17 16:54:19 -0400

    fixed crash

--- app/src/main/java/com/tesfayeabel/lolchat/ui/ChatActivity.java
+++ app/src/main/java/com/tesfayeabel/lolchat/ui/ChatActivity.java
@@ -124,10 +124,13 @@ public class ChatActivity extends Activity implements ServiceConnection, SharedP
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
-        String[] messages = preferences.getString(key, "").split("\n");
-        MessageAdapter adapter = (MessageAdapter) conversation.getAdapter();
-        adapter.addMessage(new Message(messages[messages.length - 1]));//get last message
-        conversation.setSelection(adapter.getCount() - 1);
+        String message = preferences.getString(key, null);
+        if(message != null) {
+            String[] messages = message.split("\n");
+            MessageAdapter adapter = (MessageAdapter) conversation.getAdapter();
+            adapter.addMessage(new Message(messages[messages.length - 1]));//get last message
+            conversation.setSelection(adapter.getCount() - 1);
+        }
     }
 
     @Override
