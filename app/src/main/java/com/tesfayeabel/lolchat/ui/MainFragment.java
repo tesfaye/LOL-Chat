a16e779f110d9f69fd34777b1c1fd8b4b2814479
Abel Tesfaye <abeln8r@cox.net>
2014-09-18 19:00:53 -0400

    added Azir

--- app/src/main/java/com/tesfayeabel/lolchat/ui/MainFragment.java
+++ app/src/main/java/com/tesfayeabel/lolchat/ui/MainFragment.java
@@ -2,7 +2,6 @@ package com.tesfayeabel.lolchat.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
