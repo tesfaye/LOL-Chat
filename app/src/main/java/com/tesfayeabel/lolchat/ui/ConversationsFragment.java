baba918fdf88af8ebb67eada648a4c0b8301f871
Abel Tesfaye <abeln8r@cox.net>
2014-09-25 17:08:37 -0400

    findview

--- app/src/main/java/com/tesfayeabel/lolchat/ui/ConversationsFragment.java
+++ app/src/main/java/com/tesfayeabel/lolchat/ui/ConversationsFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.github.theholywaffle.lolchatapi.LolChat;
 import com.tesfayeabel.lolchat.R;
@@ -12,9 +13,13 @@ import com.tesfayeabel.lolchat.R;
  * Created by Abel Tesfaye on 9/21/2014.
  */
 public class ConversationsFragment extends LOLChatFragment {
+
+    private ListView listView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_lolchat_conversations, container, false);
+        listView = (ListView) view.findViewById(R.id.list);
         return view;
     }
 
