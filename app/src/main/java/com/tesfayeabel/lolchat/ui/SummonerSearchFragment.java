fda65b6466c6d9fc60946afbc95674494f704492
Abel Tesfaye <abeln8r@cox.net>
2014-10-19 19:20:37 -0400

    search button works

--- app/src/main/java/com/tesfayeabel/lolchat/ui/SummonerSearchFragment.java
+++ app/src/main/java/com/tesfayeabel/lolchat/ui/SummonerSearchFragment.java
@@ -2,11 +2,14 @@ package com.tesfayeabel.lolchat.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.github.theholywaffle.lolchatapi.LolChat;
@@ -20,53 +23,64 @@ import jriot.objects.Summoner;
  */
 public class SummonerSearchFragment extends LOLChatFragment {
 
-    private Button button;
-    private EditText editText;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_lolchat_search, container, false);
-        button = (Button) view.findViewById(R.id.button);
-        editText = (EditText) view.findViewById(R.id.editText);
+        Button button = (Button) view.findViewById(R.id.button);
+        final EditText editText = (EditText) view.findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lookUpSummoner(editText.getText().toString());
+            }
+        });
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    lookUpSummoner(textView.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
         return view;
     }
 
     @Override
     public void onChatConnected(final LolChat chat) {
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final String player = editText.getText().toString();
-                if (!player.isEmpty()) {
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                final Summoner summoner = chat.getRiotApi().getSummoner(player.replace(" ", ""));
-                                getActivity().runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        if (summoner != null) {
-                                            Intent intent = new Intent(getActivity(), ProfileActivity.class);
-                                            intent.putExtra("player", player);
-                                            startActivity(intent);
-                                        } else {
-                                            Toast.makeText(getActivity().getApplicationContext(), "Summoner not found", Toast.LENGTH_SHORT).show();
-                                        }
-                                    }
-                                });
-                            } catch (JRiotException e) {
-                                getActivity().runOnUiThread(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        Toast.makeText(getActivity().getApplicationContext(), "Summoner not found", Toast.LENGTH_SHORT).show();
-                                    }
-                                });
+    }
+
+    private void lookUpSummoner(final String name) {
+        final LOLChatActivity activity = (LOLChatActivity) getActivity();
+        if(activity.getLolChat() != null && !name.isEmpty()) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        final Summoner summoner = activity.getLolChat().getRiotApi().getSummoner(name.replace(" ", ""));
+                        activity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (summoner != null) {
+                                    Intent intent = new Intent(activity, ProfileActivity.class);
+                                    intent.putExtra("player", summoner.getName());
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(activity, "Summoner not found", Toast.LENGTH_SHORT).show();
+                                }
                             }
-                        }
-                    }).start();
+                        });
+                    } catch (JRiotException e) {
+                        activity.runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(activity, "Summoner not found", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
                 }
-            }
-        });
+            }).start();
+        }
     }
 }
